8de48be8d4f846df7c1e35d26f750baa1127083f
Walisson Aguirra <walissonaguirra@proton.me>
2024-11-11 17:30:34 -0300

    test: Testando page response text

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,11 @@ Route::get('/', function () {
 Route::get('/403', function () {
 
     abort_if(true, 403);
+});
+
+
+Route::get('/products', function () {
+    return view('products', [
+        'products' => Product::all()
+    ]);
 });
